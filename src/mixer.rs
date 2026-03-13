6d64aa6d1eae1cbe8959fda5c0e9f094aa329531
Benjamin Roussel <benjamin.roussel@esa.int>
2026-03-13 22:15:53 +0100

    feat: toggle_comp + meter history

--- src/mixer.rs
+++ src/mixer.rs
@@ -1,8 +1,9 @@
 use anyhow::{Context,Result};
 use tokio::net::{UdpSocket,ToSocketAddrs};
-use tokio::time::{self, Duration};
+use tokio::time::{self, Duration, Instant};
 use tokio::sync::Mutex;
 use rosc::{OscMessage, OscPacket, OscType};
+use std::collections::VecDeque;
 
 #[derive(Debug)]
 struct Channel {
@@ -18,10 +19,49 @@ pub struct MixerState{
     channels: [Channel;16],
 }
 
+const MIN_METER_VAL: f32 = -128.0;
+#[derive(Debug)]
+pub struct MeterHistory{
+    history: VecDeque<f32>,
+    max_history_size: usize,
+
+    current_minute_peak: f32,
+    last_reset: Instant,
+}
+
+impl MeterHistory {
+    pub fn new(minutes: usize) -> Self {
+        Self {
+            history: VecDeque::with_capacity(minutes),
+            max_history_size: minutes,
+            current_minute_peak: MIN_METER_VAL, // Start at silence
+            last_reset: Instant::now(),
+        }
+    }
+
+    pub fn new_data(&mut self, meter: f32) {
+        if meter > self.current_minute_peak {
+            self.current_minute_peak = meter
+        }
+
+        if self.last_reset.elapsed() >= Duration::from_secs(60) {
+            if self.history.len() >= self.max_history_size {
+                self.history.pop_front();
+            }
+            self.history.push_back(self.current_minute_peak);
+
+            // Reset for the new minute
+            self.current_minute_peak = MIN_METER_VAL;
+            self.last_reset = Instant::now();
+        }
+    }
+}
+
 #[derive(Debug)]
 pub struct Mixer {
     socket: UdpSocket,
-    state: Mutex<MixerState>
+    state: Mutex<MixerState>,
+    meter: Mutex<MeterHistory>,
 }
 
 impl Mixer {
@@ -39,8 +79,9 @@ impl Mixer {
 
         let channels = channels.map(|c| c.unwrap());
         let state = Mutex::new(MixerState{lr_fader, lr_on, channels});
+        let meter = Mutex::new(MeterHistory::new(30));
 
-        Ok(Self{socket, state})
+        Ok(Self{socket, state, meter})
     }
 
     async fn update(&self, addr: &str, args: Vec<OscType>) -> Result<()> {
@@ -61,16 +102,49 @@ impl Mixer {
                 _ => ()
             }
         }
-        println!("{:?}", self);
         Ok(())
     }
+
+    async fn update_meter(&self, args: Vec<OscType>) -> Result<()> {
+        let first_arg = args.get(0).context("Missing arg in /meters/0")?;
+
+        let bytes = match first_arg {
+            OscType::Blob(b) => b,
+            _ => anyhow::bail!("Argument is not a blob"),
+        };
+        let meter_data = &bytes[4..]; 
+        let values: Vec<f32> = meter_data
+            .chunks_exact(2)
+            .map(|chunk| {
+                // XR18 uses Little Endian for these 16-bit values
+                let raw = i16::from_le_bytes([chunk[0], chunk[1]]);
+                raw as f32 / 256.0
+            })
+            .collect();
+        let val = values
+            .get(4..5).context("Wrong size")?
+            .iter()
+            .copied()
+            .max_by(|a, b| a.total_cmp(b))
+            .unwrap_or(MIN_METER_VAL);
+
+        let mut meter_history = self.meter.lock().await;
+        meter_history.new_data(val);
+        Ok(())
+    }
+
 
     pub async fn heartbeat_loop(&self) -> Result<()> {
         let mut interval = time::interval(Duration::from_secs(8));
         loop {
             interval.tick().await;
-            println!("Send /xremote");
-            send_osc(&self.socket, "/xremote", vec![]).await?
+            send_osc(&self.socket, "/xremote", vec![])
+                .await?;
+            send_osc(
+                &self.socket,
+                "/meters",
+                vec![OscType::String("/meters/0".to_string()), OscType::Int(31)]
+            ).await?;
         }
     }
 
@@ -80,9 +154,11 @@ impl Mixer {
         let len = self.socket.recv(&mut buf).await?;
         if let Ok((_, OscPacket::Message(msg))) = rosc::decoder::decode_udp(&buf[..len]) {
             match msg.addr.as_str() {
-                "/meters" => { /* Logic for sound levels */ },
+                "/meters/0" => {
+                    self.update_meter(msg.args).await?;
+                },
                 addr => {
-                    self.update(addr, msg.args).await?
+                    self.update(addr, msg.args).await?;
                 }
             }
         }
@@ -115,16 +191,29 @@ impl Mixer {
         send_osc(&self.socket, "/lr/mix/on", vec![OscType::Int(val)]).await
     }
 
+    async fn toggle_comp(&self, chan: usize) -> Result<()> {
+        let state = self.state.lock().await;
+        let channel = state
+            .channels.get(chan).context("Wrong channel number")?;
+        let val = if channel.compressed { 0 } else { 1 };
+
+        let addr = format!("/ch/{:02}/dyn/on", chan);
+
+        send_osc(&self.socket, &addr, vec![OscType::Int(val)]).await
+    }
+
     pub async fn exec_cmd(&self, cmd: &str, val: &[&str]) -> Result<()> {
         match cmd {
             "inc-vol" => {
-                println!("Here mute");
                 let inc_vol: f64 = val[0].parse()?;
                 self.inc_fader(inc_vol).await?;
             },
             "toggle-mute" => {
-                println!("Toggle mute");
                 self.toggle_mute().await?;
+            },
+            "toggle-comp" => {
+                let chan: usize = val[0].parse()?;
+                self.toggle_comp(chan).await?;
             },
             _ => println!("Unknown command {}", cmd)
         }
@@ -203,7 +292,6 @@ async fn recv_osc(socket: &UdpSocket) -> Result<OscMessage> {
 }
 
 async fn recv_one_var_osc(socket: &UdpSocket, addr: &str) -> Result<OscType> {
-    println!("Sending {}", addr);
     send_osc(socket, addr, vec![]).await?;
     let msg = recv_osc(socket).await?;
     if msg.addr != addr {
